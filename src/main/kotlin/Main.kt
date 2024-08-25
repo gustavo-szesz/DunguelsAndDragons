245131df462d1a70f184fb5d96498e4ef785677a
gustavo szesz <szeszgustavo@gmail.com>
2024-08-25 18:40:30 -0300

    Commit exit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,74 @@
+// Main.kt
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.example.entity.Personagem
+import org.example.entity.classes.Classe
+import org.example.entity.classes.Raca
+import org.example.entity.classes.specialized.dataclass.AtaqueExtra
+import org.example.entity.classes.specialized.dataclass.DefesaExtra
+import java.util.Scanner
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Criação de Personagem")
+    print("Nome: ")
+    val nome = scanner.nextLine()
+
+    print("Força: ")
+    val forca = scanner.nextInt()
+
+    print("Destreza: ")
+    val destreza = scanner.nextInt()
+
+    print("Constituição: ")
+    val constituicao = scanner.nextInt()
+
+    print("Inteligência: ")
+    val inteligencia = scanner.nextInt()
+
+    print("Sabedoria: ")
+    val sabedoria = scanner.nextInt()
+
+    print("Carisma: ")
+    val carisma = scanner.nextInt()
+
+    scanner.nextLine() // Consume newline
+
+    print("Classe: ")
+    val classeNome = scanner.nextLine()
+
+    val habilidades = listOf(AtaqueExtra(), DefesaExtra())
+
+    class Guerreiro : Classe {
+        override val nome = "Guerreiro"
+        override val habilidades = habilidades
     }
+
+    class Humano : Raca() {
+        override val nome = "Humano"
+        override val atributos = mapOf("Força" to 2, "Destreza" to 1)
+    }
+
+    val classe = Guerreiro()
+    val raca = Humano()
+
+    val personagem = Personagem(
+        nome = nome,
+        forca = forca,
+        destreza = destreza,
+        constituicao = constituicao,
+        inteligencia = inteligencia,
+        sabedoria = sabedoria,
+        carisma = carisma,
+        classe = classe,
+        raca = raca,
+        deslocamento = TODO(),
+        idiomas = TODO(),
+        pontosDeVida = TODO(),
+        habilidades = TODO(),
+        nivel = TODO()
+    )
+
+    println("Personagem criado: $personagem")
 }
--- src/main/kotlin/entity/Personagem.kt
+++ src/main/kotlin/entity/Personagem.kt
@@ -1,13 +1,12 @@
 package org.example.entity
 
-import org.example.entity.classes.Classe
 import org.example.entity.classes.Raca
 import org.example.entity.classes.specialized.interfaces.Habilidade
 
 class Personagem(
     val nome: String,
     val raca: Raca,
-    val classe: Classe<Any?>,
+    val classe: Any,
     var deslocamento: Double,
     val idiomas: Any,
     var forca: Int = 10,
